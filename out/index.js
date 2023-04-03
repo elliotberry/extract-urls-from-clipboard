4a7ed577d0e60804c7efdd65ff1405a078b407ac
Elliot Berry <elliot@daddy.cat>
2023-04-03 19:08:36 -0400

    progress

--- out/index.js
+++
@@ -1,9 +0,0 @@
-import extractUrls from "extract-urls";
-import clipboard from 'clipboardy';
-
-
-
-let cliptext = clipboard.readSync();
-let urls = extractUrls(cliptext);
-console.log(urls.join('\n'));
-//=> ['https://github.com/huckbit/extract-urls', 'https://www.npmjs.com/package/extract-urls']
